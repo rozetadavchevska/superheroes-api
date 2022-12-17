a722a10ba0a2f22d013cefff28c7522bd2106ae1
rozetadavchevska <rozetadavcevska@gmail.com>
2022-12-17 15:07:41 +0100

    Created database

---
+++ backend/backend/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){ }
+
+        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
+    }
+}
